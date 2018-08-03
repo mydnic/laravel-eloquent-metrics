5093c2bf9cb77c858797d1baf68d07e93add9be6
Clément Rigo <rigoclement@mydnic.be>
2018-08-04 01:20:12 +0200

    init

---
+++ src/MetricsServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Mydnic\Metrics;
+
+use Illuminate\Support\ServiceProvider;
+
+class MetricsServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
